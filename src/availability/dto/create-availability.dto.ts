a1d8113189413948e552ca787984952ae5cd44c0
Anushiya <anushiyacse43@gmail.com>
2026-02-16 12:53:03 +0530

    slot duration added

--- src/availability/dto/create-availability.dto.ts
+++ src/availability/dto/create-availability.dto.ts
@@ -2,9 +2,15 @@ import { DayOfWeek } from "src/generated/prisma/enums"
 
 
 export class CreateAvailabilityDto {
-dayOfWeek : DayOfWeek[]
-startTime : string
-endTime : string
-slotDuration : number
-maxCount : number
+	dayOfWeek : DayOfWeek[];
+	/**
+	 * Start time in 24-hour format (HH:mm), e.g., '13:00' for 1pm
+	 */
+	startTime : string;
+	/**
+	 * End time in 24-hour format (HH:mm), e.g., '16:00' for 4pm
+	 */
+	endTime : string;
+	slotDuration : number;
+	maxCount : number;
 }
